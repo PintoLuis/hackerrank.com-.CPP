f891ad5bff6825d362fa76363b106b9508026489
Luis Pinto <luis.pinto1@vodafone.pt>
2016-11-03 18:32:25 +0000

    Basics

---
+++ Basic Data Types.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    int i;
+    long l;
+    long long ll;
+    char c;
+    float f;
+    long double d;
+    
+    long double r= -5279235.721231465;
+    cout.precision(17);
+    
+    //cout << r;
+    cin >> i >> l >> ll >> c >> f >> d; 
+    
+    //scanf("%d %ld %lld %c %f %lf", &i, &l, &ll, &c, &f, &d);  
+    
+    cout << i << "\n" << l << "\n" << ll << "\n" << c << "\n" << f << "\n" << d;
+    
+    
+     
+    
+    return 0;
+}
---
+++ Input and Output.h
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int f, s, t;    
+    int sum=0;
+    
+    cin >> f >> s >> t;
+
+    sum = f + s + t;
+    
+    cout << sum << endl;
+      
+    return 0;
+    
+}
