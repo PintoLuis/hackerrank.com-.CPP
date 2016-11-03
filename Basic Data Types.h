ec7a6191137dd674b85876a87c604197a34a58ec
Luis Pinto <luis.pinto1@vodafone.pt>
2016-11-03 18:35:01 +0000

    Changes on comments

--- Basic Data Types.h
+++ Basic Data Types.h
@@ -11,18 +11,11 @@ int main() {
     float f;
     long double d;
     
-    long double r= -5279235.721231465;
-    cout.precision(17);
     
-    //cout << r;
     cin >> i >> l >> ll >> c >> f >> d; 
     
-    //scanf("%d %ld %lld %c %f %lf", &i, &l, &ll, &c, &f, &d);  
-    
+	cout.precision(17);
     cout << i << "\n" << l << "\n" << ll << "\n" << c << "\n" << f << "\n" << d;
-    
-    
-     
     
     return 0;
 }
